b4ce550d2252d5556f0b7a3c3621a1dc73b8bb83
Raniel <rnldourado@gmail.com>
2021-11-15 16:13:51 -0300

    MVP page finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+
+  const [city, setCity] = useState('');
+
+  const [weatherForecast, setWeatherForecast] = useState(null);
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  }
+
+  const searchForecastWeather = () => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=37cce82af5f441e7a5c71337211511&q=${city}&lang=pt`)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setWeatherForecast(data);
+        console.log(data);
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <a href="locahost" className="navbar-brand">EBAC Weather</a>
+      </nav>
+
+      <main className="container" id="search">
+        <div className="jumbotron">
+          <h1>Verifique agora a previsão do tempo na sua cidade</h1>
+          <p className="lead">Digite a sua cidade no campo abaixo e em seguida clique em porcurar.</p>
+
+          <div className="mb-4 ">
+            <div className="col-sm-6">
+              <input type="text" className="form-control" value={city} onChange={handleCityChange} />
+            </div>
+          </div>
+
+          <button className="btn btn-lg btn-primary" onClick={searchForecastWeather}>Pesquisar</button>
+
+          {
+            weatherForecast ? (
+              <div className="mt-4 d-flex align-items-center">
+                <div className="col-sm-1">
+                  <img src={weatherForecast.current.condition.icon} alt={weatherForecast.current.condition.text} />
+                </div>
+                <div>
+                  <h3>
+                    Hoje o dia está: {weatherForecast.current.condition.text} em {weatherForecast.location.name}, {weatherForecast.location.region}, {weatherForecast.location.country}
+                  </h3>
+                  <p className="lead">
+                    Temperatura: {weatherForecast.current.temp_c} ºC
+                  </p>
+                </div>
+              </div>
+
+            ) : null
+          }
+
+        </div>
+      </main>
     </div>
   );
 }
